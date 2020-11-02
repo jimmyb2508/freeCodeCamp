07e6f70f357b2c5dfc9e13a0795de54a5e42147d
James Barrington <james.barrington1985@gmail.com>
2020-11-02 21:11:27 +0000

    Completed Repeat a String KATA

---
+++ JavaScript_Algorithims_and_Data_Structures/Basic_Algorithm_Scripting/Repeat a String Repeat a String.js
@@ -0,0 +1,74 @@
+function repeatStringNumTimes(str, num) {
+  // Step 1. Create an empty string that will host the repeated string
+  let repeatedString = "";
+
+  // Step 2. Set the While loop with (times > 0) as the condition to check
+  while(num > 0){
+    repeatedString += str;
+    num--;
+  }
+    /* While loop logic
+                      Condition       T/F       repeatedString += string      repeatedString        times
+    First iteration    (3 > 0)        true            "" + "abc"                  "abc"               2
+    Second iteration   (2 > 0)        true           "abc" + "abc"               "abcabc"             1
+    Third iteration    (1 > 0)        true          "abcabc" + "abc"            "abcabcabc"           0
+    Fourth iteration   (0 > 0)        false
+    }
+  */
+
+  // Step 3. Return the repeated string
+  return repeatedString;
+}
+
+repeatStringNumTimes("abc", 3);
+console.log(repeatStringNumTimes("abc", 3));
+
+function repeatStringXNumberOfTimes(str, num) {
+  // Step 1. Check if num is negative and return an empty string if true
+  if(num < 0){
+    return "";
+  }
+
+  // Step 2. Check if num equals to 1 and return the string itself if it's the case.
+  if(num === 1){
+    return str;
+  }
+
+  // Step 3. Use recursion
+  else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+    /* 
+    First Part of the recursion method
+    You need to remember that you won’t have just one call, you’ll have several nested calls
+                     times       string + repeatStringNumTimes(string, times - 1)
+      1st call         3                 "abc" + ("abc", 3 - 1)
+      2nd call         2                 "abc" + ("abc", 2 - 1)
+      3rd call         1                 "abc" => if (times === 1) return string;
+      4th call         0                  ""   => if (times <= 0) return "";
+    Second part of the recursion method
+      4th call will return      ""
+      3rd call will return     "abc"
+      2nd call will return     "abc"
+      1st call will return     "abc"
+    The final call is a concatenation of all the strings
+    return "abc" + "abc" + "abc"; // return "abcabcabc";
+  */
+}
+
+console.log(repeatStringXNumberOfTimes("abc", 3));
+
+// Repeat a String using ES6 repeat() method
+
+function repeatString(str, num){
+  //Step 1. If num is positive, return the repeated string
+  if(num > 0){
+    return str.repeat(num);
+  }
+  //Step 2. Else if times is negative, return an empty string if true
+  else {
+    return "";
+  }
+}
+
+console.log(repeatString("abc", 3));
